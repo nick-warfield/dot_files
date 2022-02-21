32947fc4b5e44ac7905b45cc5eb15e0269271b37
Nicholas Warfield <nick@nkwarfield.io>
2022-02-20 19:35:08 -0800

    Fix remember_folds autocmd the autocmd was matching unnamed files and causing bugs with telescpe

--- nvim/init.lua
+++ nvim/init.lua
@@ -349,18 +349,18 @@ require('orgmode').setup_ts_grammar()
 
 -- Tree-sitter configuration
 require'nvim-treesitter.configs'.setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    --disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
-    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
-  },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
+	-- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+	highlight = {
+		enable = true,
+		--disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+		additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
+	},
+	ensure_installed = {'org'}, -- Or run :TSUpdate org
 }
 
 require('orgmode').setup({
-  org_agenda_files = { '~/Documents/org/*' },
-  org_default_notes_file = '~/Documents/org/refile.org',
+	org_agenda_files = { '~/Documents/org/*' },
+	org_default_notes_file = '~/Documents/org/refile.org',
 })
 
 -- Remaining Vimscript to convert
@@ -375,9 +375,9 @@ autocmd! BufEnter *.tpp let b:fswitchdst = 'hpp,h' | let b:fswitchlocs = '../inc
 
 " Folds Config
 augroup remember_folds
-autocmd!
-autocmd BufWinLeave * mkview
-autocmd BufWinEnter * silent! loadview
+	autocmd!
+	autocmd BufWinLeave ?* mkview
+	autocmd BufWinEnter ?* silent! loadview
 augroup END
 
 function! NeatFoldText()
@@ -542,4 +542,4 @@ let g:livepreview_previewer = 'qpdfview'
 let g:vim_markdown_follow_anchor = 1
 let g:python3_host_prog='/usr/bin/python'
 
-]]
+	]]

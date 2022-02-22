6dd864fea4f4b2c2605a782437f97ad2276389de
Nick Warfield <nick@nkwarfield.io>
2022-02-21 20:55:56 -0800

    Add basic fish config

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+SETUVAR BROWSER:/bin/firefox
+SETUVAR _fish_abbr_b:bat
+SETUVAR _fish_abbr_r:ranger
+SETUVAR _fish_abbr_rm:echo\x20\x22Did\x20you\x20mean\x20trash\x20\x28t\x29\x3f\x3b\x20false
+SETUVAR _fish_abbr_t:trash
+SETUVAR _fish_abbr_v:nvim
+
+SETUVAR fish_key_bindings:fish_vi_key_bindings
+
+function jl
+	jq -C | less -R
+end
